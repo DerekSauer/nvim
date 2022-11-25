aff2e46bd7f831f6218e4c92de9774f99a91e888
Derek Sauer <dereksauer.ca@gmail.com>
2022-11-24 22:52:54 -0500

    Added Nightfox colorscheme.

---
+++ lua/plugin-config/nightfox.lua
@@ -0,0 +1,21 @@
+local config = {
+    options = {
+        dim_inactive = true,
+        styles = {
+            comments = "NONE",
+            conditionals = "NONE",
+            constants = "NONE",
+            functions = "NONE",
+            keywords = "NONE",
+            numbers = "NONE",
+            operators = "NONE",
+            strings = "NONE",
+            types = "NONE",
+            variables = "NONE",
+        },
+        modules = { },
+    },
+}
+
+require("nightfox").setup(config)
+vim.cmd("colorscheme duskfox")
