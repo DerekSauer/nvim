0b45fb52b873c46ec7511079ae096704b36cad0f
Derek Sauer <dereksauer.ca@gmail.com>
2022-11-27 21:05:04 -0500

    Added styling to some text categories.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,9 @@
 local packer = require("packer")
 local globals = require("globals")
 
+-- Remove depricated legacy commands from neo-tree prior to loading it
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
 -- Install Packer if needed
 local packer_bootstrap = require("bootstrap").ensure_packer()
 
@@ -11,12 +14,41 @@ packer.startup({
         -- Packer will manage itself
         use "wbthomason/packer.nvim"
 
+        -- Impatient - Improve startup time by caching modules
+        -- https://github.com/lewis6991/impatient.nvim
+        use {
+            "lewis6991/impatient.nvim",
+            after = "packer.nvim",
+            config = function() require("impatient") end
+        }
+
         -- Nightfox colorscheme
         -- https://github.com/EdenEast/nightfox.nvim
         use {
             "EdenEast/nightfox.nvim",
             after = "packer.nvim",
             config = function() require("plugin-config/nightfox") end
+        }
+
+        -- Neo-tree file drawer
+        -- https://github.com/nvim-neo-tree/neo-tree.nvim
+        use {
+            "nvim-neo-tree/neo-tree.nvim",
+            branch = "v2.x",
+            requires = { 
+                "nvim-lua/plenary.nvim",
+                "nvim-tree/nvim-web-devicons",
+                "MunifTanjim/nui.nvim",
+            },
+            config = function() require("plugin-config/neo-tree") end
+        }
+
+        -- Treesitter configurations and abstraction layer
+        -- https://github.com/nvim-treesitter/nvim-treesitter
+        use {
+            "nvim-treesitter/nvim-treesitter",
+            run = function() vim.cmd("TSUpdate<CR>") end,
+            config = function() require("plugin-config/treesitter") end
         }
     end,
 
