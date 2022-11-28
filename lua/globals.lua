f14138859d0cc716ce53646c2461ac8a34e5118a
Derek Sauer <dereksauer.ca@gmail.com>
2022-11-27 21:38:19 -0500

    Added diagnostics icons to global config.

--- lua/globals.lua
+++ lua/globals.lua
@@ -3,4 +3,10 @@ local M = {}
 -- Prefered border style for float windows
 M.border_style = "single"
 
+-- Define icons for diagnostics
+vim.fn.sign_define("DiagnosticSignError", {text = " ", texthl = "DiagnosticSignError"})
+vim.fn.sign_define("DiagnosticSignWarn",  {text = " ", texthl = "DiagnosticSignWarn"})
+vim.fn.sign_define("DiagnosticSignInfo",  {text = " ", texthl = "DiagnosticSignInfo"})
+vim.fn.sign_define("DiagnosticSignHint",  {text = "", texthl = "DiagnosticSignHint"})
+
 return M
