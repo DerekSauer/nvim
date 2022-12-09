c5700122a1f8f625b60cff773d4404a192de84ed
Derek Sauer <dereksauer.ca@gmail.com>
2022-12-08 21:59:24 -0500

    Simplify file explorer usage.
    I've never actually used neo-tree's buffer and git features. I already
    have a better interface for managing buffers using nvim's own
    functionality and some keybinds and neo-tree already displays git
    statuses as icons.

--- lua/plugin-config/neo-tree.lua
+++ lua/plugin-config/neo-tree.lua
@@ -15,9 +15,6 @@ if neo_tree then
         close_if_last_window = true,
         popup_border_style = globals.border_style,
         use_libuv_file_watcher = true,
-        source_selector = {
-            winbar = true
-        },
         window = {
             position = "left",
             width = 32
@@ -26,15 +23,5 @@ if neo_tree then
 
     neo_tree.setup(config)
 
-    vim.keymap.set('n', "<leader>ee", function() vim.cmd("Neotree focus filesystem left") end, { silent = true, desc = "Open file explorer." })
-    vim.keymap.set('n', "<leader>ec", function() vim.cmd("Neotree close filesystem left") end, { silent = true, desc = "Close file explorer." })
-    vim.keymap.set('n', "<leader>eb", function() vim.cmd("Neotree focus buffers left") end, { silent = true, desc = "Show buffers in file explorer." })
-    vim.keymap.set('n', "<leader>eg", function() vim.cmd("Neotree focus git_status left") end, { silent = true, desc = "Show git status in file explorer." })
-    vim.keymap.set('n', "<leader>er", function() vim.cmd("Neotree reveal left") end, { silent = true, desc = "Focus on current file in file explorer." })
-
-    -- Add keymap group for neo-tree to which-key
-    local loaded, whichkey = pcall(require, "which-key")
-    if loaded then
-        whichkey.register({ e = { name = "File Explorer" } }, { prefix = "<leader>" })
-    end
+    vim.keymap.set('n', "<leader>e", function() vim.cmd("Neotree toggle") end, { silent = true, desc = "Toggle file explorer." })
 end
