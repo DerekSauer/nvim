074515f6bbff64627703a3b2f2f899c62b32d3e8
Derek Sauer <dereksauer.ca@gmail.com>
2022-12-12 10:52:05 -0500

    Disable 'luassert' message when opening lua files.

--- lua/plugin-config/lsp.lua
+++ lua/plugin-config/lsp.lua
@@ -23,6 +23,7 @@ if loaded then
                 },
                 workspace = {
                     library = vim.api.nvim_get_runtime_file("", true),
+                    checkThirdParty = false
                 },
                 telemetry = {
                     enable = false,
