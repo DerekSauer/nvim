18e74fabeacf7305f894843534d1d65d79e18830
Derek Sauer <dereksauer.ca@gmail.com>
2022-12-16 14:51:03 -0500

    Added autocommand to reload launch.json on change.

--- lua/plugin-config/dap.lua
+++ lua/plugin-config/dap.lua
@@ -332,7 +332,21 @@ if dap_ok then
         end,
     }
 
-    require("dap.ext.vscode").load_launchjs(nil, { codelldb = { "c", "cpp", "rust" } })
+    -- Map adapters to configurations for load_launchjs()
+    local adapter_map = {
+        codelldb = { "c", "cpp", "rust" },
+    }
+
+    -- Parse a ".vscode/launch.js" file if one is present in the workspace
+    require("dap.ext.vscode").load_launchjs(nil, adapter_map)
+
+    -- Autocommand to reparse the launch.json file if it changes
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        pattern = { ".vscode/launch.json" },
+        callback = function()
+            require("dap.ext.vscode").load_launchjs(nil, adapter_map)
+        end,
+    })
 else
     vim.notify("Failed to load plugin: dap", vim.log.levels.ERROR)
 end
