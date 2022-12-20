3b8d2e619401155827cdabde2b5a1e1da08b7803
Derek Sauer <dereksauer.ca@gmail.com>
2022-12-20 07:51:48 -0500

    Load frecency only on not-windows for now.

--- lua/plugin-config/telescope.lua
+++ lua/plugin-config/telescope.lua
@@ -12,46 +12,77 @@ if telescope_ok then
     }
 
     telescope.setup(config)
-    telescope.load_extension("frecency")
     telescope.load_extension("project")
 
+    -- Having trouble making the sqlite3 dev libs available for this extension on windows
+    -- load it only on *nix for now
+    if vim.fn.has("win32") == 0 then telescope.load_extension("frecency") end
+
     local dap_loaded, _ = pcall(require, "dap")
-    if dap_loaded then
-        telescope.load_extension("dap")
-    end
+    if dap_loaded then telescope.load_extension("dap") end
 
     -- Key mappings
-    vim.keymap.set("n", "<leader>ff", function()
-        require("telescope.builtin").find_files()
-    end, { silent = true, desc = "Find files" })
-    vim.keymap.set("n", "<leader>fs", function()
-        require("telescope.builtin").grep_string()
-    end, { silent = true, desc = "String grep" })
-    vim.keymap.set("n", "<leader>fg", function()
-        require("telescope.builtin").live_grep()
-    end, { silent = true, desc = "Live grep" })
-    vim.keymap.set("n", "<leader>fb", function()
-        require("telescope.builtin").buffers()
-    end, { silent = true, desc = "Buffers" })
-    vim.keymap.set("n", "<leader>fm", function()
-        require("telescope.builtin").man_pages()
-    end, { silent = true, desc = "Man pages" })
-    vim.keymap.set("n", "<leader>fq", function()
-        require("telescope.builtin").quickfix()
-    end, { silent = true, desc = "Quickfix list" })
-    vim.keymap.set("n", "<leader>fl", function()
-        require("telescope.builtin").loclist()
-    end, { silent = true, desc = "Location list" })
-    vim.keymap.set("n", "<leader>f/", function()
-        require("telescope.builtin").keymaps()
-    end, { silent = true, desc = "Keymap help" })
+    vim.keymap.set(
+        "n",
+        "<leader>ff",
+        function() require("telescope.builtin").find_files() end,
+        { silent = true, desc = "Find files" }
+    )
+    vim.keymap.set(
+        "n",
+        "<leader>fs",
+        function() require("telescope.builtin").grep_string() end,
+        { silent = true, desc = "String grep" }
+    )
+    vim.keymap.set(
+        "n",
+        "<leader>fg",
+        function() require("telescope.builtin").live_grep() end,
+        { silent = true, desc = "Live grep" }
+    )
+    vim.keymap.set(
+        "n",
+        "<leader>fb",
+        function() require("telescope.builtin").buffers() end,
+        { silent = true, desc = "Buffers" }
+    )
+    vim.keymap.set(
+        "n",
+        "<leader>fm",
+        function() require("telescope.builtin").man_pages() end,
+        { silent = true, desc = "Man pages" }
+    )
+    vim.keymap.set(
+        "n",
+        "<leader>fq",
+        function() require("telescope.builtin").quickfix() end,
+        { silent = true, desc = "Quickfix list" }
+    )
+    vim.keymap.set(
+        "n",
+        "<leader>fl",
+        function() require("telescope.builtin").loclist() end,
+        { silent = true, desc = "Location list" }
+    )
+    vim.keymap.set(
+        "n",
+        "<leader>f/",
+        function() require("telescope.builtin").keymaps() end,
+        { silent = true, desc = "Keymap help" }
+    )
 
-    vim.keymap.set("n", "<leader>fr", function()
-        require("telescope").extensions.frecency.frecency()
-    end, { silent = true, desc = "Recent files" })
-    vim.keymap.set("n", "<leader>fp", function()
-        require("telescope").extensions.project.project()
-    end, { silent = true, desc = "Projects" })
+    vim.keymap.set(
+        "n",
+        "<leader>fr",
+        function() require("telescope").extensions.frecency.frecency() end,
+        { silent = true, desc = "Recent files" }
+    )
+    vim.keymap.set(
+        "n",
+        "<leader>fp",
+        function() require("telescope").extensions.project.project() end,
+        { silent = true, desc = "Projects" }
+    )
 
     -- Add to which-key categories
     local whickey_loaded, whichkey = pcall(require, "which-key")
