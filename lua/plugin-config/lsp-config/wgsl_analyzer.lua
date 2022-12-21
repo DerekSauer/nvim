b6ce66a301cc5c872e6086f56def77a4c392c852
Derek Sauer <dereksauer.ca@gmail.com>
2022-12-21 13:53:58 -0500

    Manually specify the binary for wgsl-analyzer.
    The default config in lsp-config is using cmd.exe to execute the lsp
    server on windows which seems to prohibit it from functioning correctly.

--- lua/plugin-config/lsp-config/wgsl_analyzer.lua
+++ lua/plugin-config/lsp-config/wgsl_analyzer.lua
@@ -1,11 +1,23 @@
 local M = {}
 
-function M.setup(lsp_zero) lsp_zero.configure("wgsl_analyzer", {}) end
+function M.setup(lsp_zero)
+    -- Get the path to `wgsl_analyzer` installed by Mason.nvim
+    local wgsl_analyzer_path = require("mason-registry")
+        .get_package("wgsl-analyzer")
+        :get_install_path() .. "/bin/"
+    local wgsl_analyzer_bin = wgsl_analyzer_path .. "wgsl_analyzer"
 
--- Autocommand to reparse the launch.json file if it changes
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-    pattern = { "*.wgsl" },
-    command = "set filetype=wgsl",
-})
+    lsp_zero.configure("wgsl_analyzer", {
+        cmd = { wgsl_analyzer_bin },
+        filetypes = { "wgsl" },
+        settings = {},
+    })
+
+    -- Autocommand to reparse the launch.json file if it changes
+    vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+        pattern = { "*.wgsl" },
+        command = "set filetype=wgsl",
+    })
+end
 
 return M
