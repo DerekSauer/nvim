8c9681f0ba26fac666948f502dbb6b95af8b1577
Derek Sauer <dereksauer.ca@gmail.com>
2023-01-06 11:01:56 -0500

    Ported deferred-clipboard to lazy.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,12 +1,5 @@
 -- Configure list of plugins and their dependencies
 return {
-    -- Sync system clipboard with Neovim
-    -- https://github.com/EtiamNullam/deferred-clipboard.nvim
-    {
-        "EtiamNullam/deferred-clipboard.nvim",
-        config = function() require("deferred-clipboard").setup() end,
-    },
-
 
     -- Lsp Setup and completions
     -- https://github.com/VonHeikemen/lsp-zero.nvim
