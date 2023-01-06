5b7604c8bdf6a6cb39095d3ef1ed4f8337295637
Derek Sauer <dereksauer.ca@gmail.com>
2023-01-06 15:20:17 -0500

    Check if the lldb debug adapter is installed.

--- lua/plugins/dap/lldb.lua
+++ lua/plugins/dap/lldb.lua
@@ -6,22 +6,30 @@ function M.setup(dap)
         .. "/extension"
     local lldb_bin = lldb_path .. "/adapter/codelldb"
 
-    -- Configure the LLDB adapter
-    dap.adapters.lldb = {
-        type = "server",
-        port = "${port}",
-        executable = {
-            command = lldb_bin,
-            args = { "--port", "${port}" },
-        },
-        enrich_config = function(config, on_config)
-            -- If the configuration(s) in `launch.json` contains a `cargo` section
-            -- send the configuration off to the cargo_inspector.
-            if config["cargo"] ~= nil then
-                on_config(require("plugins/dap/cargo_inspector").inspect(config))
-            end
-        end,
-    }
+    if vim.fn.executable(lldb_bin) == 1 then
+        -- Configure the LLDB adapter
+        dap.adapters.lldb = {
+            type = "server",
+            port = "${port}",
+            executable = {
+                command = lldb_bin,
+                args = { "--port", "${port}" },
+            },
+            enrich_config = function(config, on_config)
+                -- If the configuration(s) in `launch.json` contains a `cargo` section
+                -- send the configuration off to the cargo_inspector.
+                if config["cargo"] ~= nil then
+                    on_config(require("plugins/dap/cargo_inspector").inspect(config))
+                end
+            end,
+        }
+    else
+        vim.notify(
+            "The codelldb debug adapter is not installed.\nPlease use Mason to install `codelldb`.",
+            vim.log.levels.ERROR
+        )
+        return {}
+    end
 end
 
 return M
