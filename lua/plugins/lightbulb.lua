8839be1e6de48a5ba3b59dd565becd0f9965575e
Derek Sauer <dereksauer.ca@gmail.com>
2023-01-09 10:19:13 -0500

    Moved lsp lightbulb into the LSP config.

--- lua/plugins/lightbulb.lua
+++
@@ -1,8 +0,0 @@
-local M = {
-    -- Lightbulb 💡icon in the gutter when code action is available.
-    -- https://github.com/kosayoda/nvim-lightbulb
-    "kosayoda/nvim-lightbulb",
-    config = function() require("nvim-lightbulb").setup({ autocmd = { enabled = true } }) end,
-}
-
-return M
