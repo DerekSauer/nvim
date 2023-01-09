1adb3d9a0e7dc45af6139bfddc48fe57ae8c69f3
Derek Sauer <dereksauer.ca@gmail.com>
2023-01-09 11:02:53 -0500

    Force redraw on symbol outline toggle to cleanup leftover chars.

--- lua/plugins/lsp/symbolsoutline.lua
+++ lua/plugins/lsp/symbolsoutline.lua
@@ -3,12 +3,10 @@ local M = {}
 function M.setup(bufnr)
     require("symbols-outline").setup()
 
-    vim.keymap.set(
-        "n",
-        "<leader>o",
-        function() vim.cmd("SymbolsOutline") end,
-        { silent = true, buffer = bufnr, desc = "Toggle symbol outline" }
-    )
+    vim.keymap.set("n", "<leader>o", function()
+        vim.cmd("SymbolsOutline")
+        vim.cmd("redraw")
+    end, { silent = true, buffer = bufnr, desc = "Toggle symbol outline" })
 end
 
 return M
