ece117b02368814fdabc1f69466fdbdf084fc223
Derek Sauer <dereksauer.ca@gmail.com>
2023-02-08 10:14:47 -0500

    Work around for poor clipboard performance on Windows.

--- lua/plugins/clipboard.lua
+++
@@ -1,8 +0,0 @@
-local M = {
-    -- Sync system clipboard with Neovim
-    -- https://github.com/EtiamNullam/deferred-clipboard.nvim
-    "EtiamNullam/deferred-clipboard.nvim",
-    config = function() require("deferred-clipboard").setup() end,
-}
-
-return M
