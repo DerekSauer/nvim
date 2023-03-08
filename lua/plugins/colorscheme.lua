150375bf14fd617408653d282c86030299742812
Derek Sauer <dereksauer.ca@gmail.com>
2023-03-08 13:12:09 -0500

    Ported colorscheme to new version of Kanagawa.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,70 +7,80 @@ local M = {
 }
 
 function M.config()
-    local kanagawa = require("kanagawa")
-    local default_colors = require("kanagawa.colors").setup()
-
-    -- Define highlight overrides
-    local hl_overrides = {
-        NavicIconsFile = { fg = default_colors.springViolet2 },
-        NavicIconsModule = { fg = default_colors.boatYellow2 },
-        NavicIconsNamespace = { fg = default_colors.springViolet2 },
-        NavicIconsPackage = { fg = default_colors.springViolet1 },
-        NavicIconsClass = { fg = default_colors.surimiOrange },
-        NavicIconsMethod = { fg = default_colors.crystalBlue },
-        NavicIconsProperty = { fg = default_colors.waveAqua2 },
-        NavicIconsField = { fg = default_colors.waveAqua1 },
-        NavicIconsConstructor = { fg = default_colors.surimiOrange },
-        NavicIconsEnum = { fg = default_colors.boatYellow2 },
-        NavicIconsInterface = { fg = default_colors.carpYellow },
-        NavicIconsFunction = { fg = default_colors.crystalBlue },
-        NavicIconsVariable = { fg = default_colors.oniViolet },
-        NavicIconsConstant = { fg = default_colors.oniViolet },
-        NavicIconsString = { fg = default_colors.springGreen },
-        NavicIconsNumber = { fg = default_colors.sakuraPink },
-        NavicIconsBoolean = { fg = default_colors.surimiOrange },
-        NavicIconsArray = { fg = default_colors.waveAqua2 },
-        NavicIconsObject = { fg = default_colors.surimiOrange },
-        NavicIconsKey = { fg = default_colors.oniViolet },
-        NavicIconsNull = { fg = default_colors.carpYellow },
-        NavicIconsEnumMember = { fg = default_colors.carpYellow },
-        NavicIconsStruct = { fg = default_colors.surimiOrange },
-        NavicIconsEvent = { fg = default_colors.surimiOrange },
-        NavicIconsOperator = { fg = default_colors.springViolet2 },
-        NavicIconsTypeParameter = { fg = default_colors.springBlue },
-        NavicText = { fg = default_colors.fujiWhite },
-        NavicSeparator = { fg = default_colors.fujiWhite },
-
-        -- Indent Blankline rainbow colors
-        IndentBlanklineIndent1 = { fg = default_colors.waveAqua1 },
-        IndentBlanklineIndent2 = { fg = default_colors.surimiOrange },
-        IndentBlanklineIndent3 = { fg = default_colors.dragonBlue },
-        IndentBlanklineIndent4 = { fg = default_colors.waveRed },
-        IndentBlanklineIndent5 = { fg = default_colors.springViolet1 },
-        IndentBlanklineIndent6 = { fg = default_colors.springGreen },
-        IndentBlanklineIndent7 = { fg = default_colors.springBlue },
-    }
-
-    local config = {
-        undercurl = true, -- enable undercurls
+    require("kanagawa").setup({
+        compile = true,
+        undercurl = true,
         commentStyle = { italic = true },
         functionStyle = {},
         keywordStyle = { italic = true },
         statementStyle = { bold = true },
         typeStyle = {},
-        variablebuiltinStyle = { italic = true },
-        specialReturn = true, -- special highlight for the return keyword
-        specialException = true, -- special highlight for exception handling keywords
-        transparent = false, -- do not set background color
-        dimInactive = true, -- dim inactive window `:h hl-NormalNC`
-        globalStatus = true, -- adjust window separators highlight for laststatus=3
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = {},
-        overrides = hl_overrides,
-        theme = "default",
-    }
+        transparent = false,
+        dimInactive = true,
+        terminalColors = true,
+        theme = "wave",
+        background = {
+            dark = "wave",
+            light = "lotus"
+        },
+        overrides = function(colors)
+            local theme = colors.theme
+            return {
+                -- Apply Kanagawa to picker menus
+                Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+                PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                PmenuSbar = { bg = theme.ui.bg_m1 },
+                PmenuThumb = { bg = theme.ui.bg_p2 },
+                -- Add kanagawa colors to Navic
+                NavicIconsFile = { link = "Directory" },
+                NavicIconsModule = { link = "TSInclude" },
+                NavicIconsNamespace = { link = "TSInclude" },
+                NavicIconsPackage = { link = "TSInclude" },
+                NavicIconsClass = { link = "Structure" },
+                NavicIconsMethod = { link = "Function" },
+                NavicIconsProperty = { link = "TSProperty" },
+                NavicIconsField = { link = "TSField" },
+                NavicIconsConstructor = { link = "@constructor" },
+                NavicIconsEnum = { link = "Identifier" },
+                NavicIconsInterface = { link = "Type" },
+                NavicIconsFunction = { link = "Function" },
+                NavicIconsVariable = { link = "@variable" },
+                NavicIconsConstant = { link = "Constant" },
+                NavicIconsString = { link = "String" },
+                NavicIconsNumber = { link = "Number" },
+                NavicIconsBoolean = { link = "Boolean" },
+                NavicIconsArray = { link = "Type" },
+                NavicIconsObject = { link = "Type" },
+                NavicIconsKey = { link = "Keyword" },
+                NavicIconsNull = { link = "Type" },
+                NavicIconsEnumMember = { link = "TSField" },
+                NavicIconsStruct = { link = "Structure" },
+                NavicIconsEvent = { link = "Structure" },
+                NavicIconsOperator = { link = "Operator" },
+                NavicIconsTypeParameter = { link = "Identifier" },
+                NavicText = { fg = theme.ui.fg },
+                NavicSeparator = { fg = theme.ui.fg },
+                -- Indent Blankline rainbow colors
+                IndentBlanklineIndent1 = { fg = theme.term[17] },
+                IndentBlanklineIndent2 = { fg = theme.term[3] },
+                IndentBlanklineIndent3 = { fg = theme.term[4] },
+                IndentBlanklineIndent4 = { fg = theme.term[5] },
+                IndentBlanklineIndent5 = { fg = theme.term[6] },
+                IndentBlanklineIndent6 = { fg = theme.term[7] },
+                IndentBlanklineIndent7 = { fg = theme.term[8] },
+            }
+        end,
+        colors = {
+            theme = {
+                all = {
+                    ui = {
+                        bg_gutter = "none"
+                    },
+                },
+            },
+        },
+    })
 
-    kanagawa.setup(config)
     vim.cmd.colorscheme("kanagawa")
 
     -- Function and keymap to swap light & dark color schemes
