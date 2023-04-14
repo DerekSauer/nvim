a08d97781fa624a5f00bee0acd06dcb7598cc6b3
Derek Sauer <dereksauer.ca@gmail.com>
2023-04-14 11:29:24 -0400

    Added zen-mode.

---
+++ lua/plugins/zenmode.lua
@@ -0,0 +1,20 @@
+local M = {
+    -- Zen-mode distraction free coding
+    -- https://github.com/folke/zen-mode.nvim
+    "folke/zen-mode.nvim",
+    event = "VeryLazy",
+}
+
+function M.config()
+    require("zen-mode").setup()
+
+    -- Key mapping
+    vim.keymap.set(
+        "n",
+        "<leader>z",
+        function() require("zen-mode").toggle() end,
+        { silent = true, desc = "Toggle zenmode" }
+    )
+end
+
+return M
