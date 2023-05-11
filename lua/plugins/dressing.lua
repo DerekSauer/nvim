974a93e0d2b6b139e05af5314f98188be67ddf10
Derek Sauer <dereksauer.ca@gmail.com>
2023-05-11 08:05:30 -0400

    Revert "Removed Dressing.nvim"
    Dressing's refusal to update was a problem on my end.
    
    This reverts commit c1fb6c5a3dc01365a62b13c76dff9a1c901b5666.

---
+++ lua/plugins/dressing.lua
@@ -0,0 +1,33 @@
+local M = {
+    -- Dressing, improve default UI
+    -- https://github.com/stevearc/dressing.nvim
+    "stevearc/dressing.nvim",
+    event = "UIEnter",
+}
+
+function M.config()
+    local globals = require("globals")
+    local config = {
+        input = {
+            enabled = true,
+            border = globals.border_style,
+            default_prompt = ">",
+        },
+        select = {
+            enabled = true,
+            backend = { "telescope" },
+            telescope = {
+                borderchars = globals.telescope_border_style,
+                layout_strategy = "cursor",
+                layout_config = {
+                    width = 0.40,
+                    height = 0.25,
+                },
+            },
+        },
+    }
+
+    require("dressing").setup(config)
+end
+
+return M
