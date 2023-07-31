e47b512084cc33399a2d1324470b46be5feab32c
Derek Sauer <dereksauer.ca@gmail.com>
2023-07-31 08:20:17 -0400

    refactor: Change borders to eliminate outside padding.

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,10 +1,10 @@
 local M = {}
 
 -- Prefered border style for float windows
-M.border_style = { "╒", "═", "╕", "│", "╛", "═", "╘", "│" }
+M.border_style = { "🭽", "▔", "🭾", "▕", "🭿", "▁", "🭼", "▏" }
 
 -- Telescope uses a different ordering for border glyphs
-M.telescope_border_style = { "═", "│", "═", "│", "╒", "╕", "╛", "╘" }
+M.telescope_border_style = { "▔", "▕", "▁", "▏", "🭽", "🭾", "🭿", "🭼" }
 
 -- Auto command group for our own commands
 M.user_au_group = vim.api.nvim_create_augroup("user_cmds", { clear = true })
