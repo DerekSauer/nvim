0b15bdbaa25bfcb0377e50f9b0cd4694f65b21c5
Derek Sauer <dereksauer.ca@gmail.com>
2023-07-31 08:54:52 -0400

    fix: Switched to a maintained colorizer plugin.

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,6 +1,23 @@
 local M = {
-    "norcalli/nvim-colorizer.lua",
-    config = function() require("colorizer").setup() end,
+    "NvChad/nvim-colorizer.lua",
 }
+
+function M.config()
+    require("colorizer").setup({
+        filetypes = { "*" },
+        user_default_options = {
+            mode = "virtualtext",
+            virtualtext = "",
+            RGB = false,
+            RRGGBB = true,
+            RRGGBBAA = true,
+            AARRGGBB = true,
+            names = false,
+            css = true,
+            sass = { enable = true, parsers = { "css" } },
+        },
+    }
+    )
+end
 
 return M
