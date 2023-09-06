b749de56111bcb5f55df0a2c298937a335932f64
Derek Sauer <dereksauer.ca@gmail.com>
2023-09-06 08:28:43 -0400

    fix: Fix TAB jumping to any snippet object instead of current snippet.

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -196,7 +196,7 @@ function M.config()
             ["<Tab>"] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_next_item()
-                elseif luasnip.expand_or_jumpable() then
+                elseif luasnip.expand_or_locally_jumpable() then
                     luasnip.expand_or_jump()
                 elseif has_words_before() then
                     cmp.complete()
@@ -209,7 +209,7 @@ function M.config()
             ["<S-Tab>"] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_prev_item()
-                elseif luasnip.jumpable(-1) then
+                elseif luasnip.locally_jumpable(-1) then
                     luasnip.jump(-1)
                 else
                     fallback()
