b6f2468ecced6433a4518d456846740aaa6a2460
Derek Sauer <dereksauer.ca@gmail.com>
2023-11-07 12:52:29 -0500

    feat: Added additional highlights to certain text fields in LSP hover text.

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -1,5 +1,80 @@
 local M = {}
 
+local float_ns = vim.api.nvim_create_namespace "lsp_float"
+
+---LSP handler that adds extra inline highlights, keymaps, and window options.
+---Code inspired from `noice` and `MariaSolOs`.
+---https://github.com/lukas-reineke/dotfiles/blob/3a1afd9bad999cc2cdde98851c2a5066f60fc193/vim/lua/lsp/init.lua#L163
+---@param handler fun(err: any, result: any, ctx: any, config: any): integer, integer
+---@return function
+local function enhanced_float_handler(handler)
+    return function(err, result, ctx, config)
+        local buf, win = handler(
+            err,
+            result,
+            ctx,
+            vim.tbl_deep_extend("force", config or {}, {
+                border = vim.g.floating_window_border,
+                max_height = math.floor(vim.o.lines * 0.5),
+                max_width = math.floor(vim.o.columns * 0.4),
+            })
+        )
+
+        if not buf or not win then
+            return
+        end
+
+        -- Extra highlights.
+        for l, line in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
+            for pattern, hl_group in pairs {
+                ["|%S-|"] = "@text.reference",
+                ["@%S+"] = "@parameter",
+                ["^%s*(Parameters:)"] = "@text.title",
+                ["^%s*(Return:)"] = "@text.title",
+                ["^%s*(See also:)"] = "@text.title",
+                ["{%S-}"] = "@parameter",
+            } do
+                local from = 1 ---@type integer?
+                while from do
+                    local to
+                    from, to = line:find(pattern, from)
+                    if from then
+                        vim.api.nvim_buf_set_extmark(buf, float_ns, l - 1, from - 1, {
+                            end_col = to,
+                            hl_group = hl_group,
+                        })
+                    end
+                    from = to and to + 1 or nil
+                end
+            end
+        end
+
+        -- Add keymaps for opening links.
+        if not vim.b[buf].markdown_keys then
+            vim.keymap.set("n", "<CR>", function()
+                -- Vim help links.
+                local url = (vim.fn.expand "<cWORD>" --[[@as string]]):match "|(%S-)|"
+                if url then
+                    return vim.cmd.help(url)
+                end
+
+                -- Markdown links.
+                local col = vim.api.nvim_win_get_cursor(0)[2] + 1
+                local from, to
+                from, to, url = vim.api.nvim_get_current_line():find "%[.-%]%((%S-)%)"
+                if from and col >= from and col <= to then
+                    vim.system({ "open", url }, nil, function(res)
+                        if res.code ~= 0 then
+                            vim.notify("Failed to open URL" .. url, vim.log.levels.ERROR)
+                        end
+                    end)
+                end
+            end, { buffer = buf, silent = true })
+            vim.b[buf].markdown_keys = true
+        end
+    end
+end
+
 function M.setup()
     local globals = require("globals")
 
@@ -25,11 +100,11 @@ function M.setup()
         },
     })
 
-    vim.lsp.handlers["textDocument/hover"] =
-        vim.lsp.with(vim.lsp.handlers.hover, { border = globals.border_style })
-
-    vim.lsp.handlers["textDocument/signatureHelp"] =
-        vim.lsp.with(vim.lsp.handlers.signature_help, { border = globals.border_style })
+    -- Assign helper windows for LSP hover text and signature help.
+    local methods = vim.lsp.protocol.Methods
+    vim.lsp.handlers[methods.textDocument_hover] = enhanced_float_handler(vim.lsp.handlers.hover)
+    vim.lsp.handlers[methods.textDocument_signatureHelp] = enhanced_float_handler(vim.lsp.handlers
+        .signature_help)
 
     -- Define icons for diagnostics
     vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
