140f357846887557cfec903bfb173652d50e3ee0
Derek Sauer <dereksauer.ca@gmail.com>
2024-05-21 08:45:50 -0400

    fix: Use latest neo-tree to prevent deprecation warnings.
    Signed-off-by: Derek Sauer <dereksauer.ca@gmail.com>

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,7 +1,6 @@
 local M = {
     -- Neo-tree file drawer.
     "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
